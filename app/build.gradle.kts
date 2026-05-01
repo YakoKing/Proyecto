b9a79cdd362eddcd5785da769839b4868a01705d
ImnotCarrot <nac3kiaa@gmail.com>
2026-05-01 12:00:44 +0200

    Implementación de reserva y guardado de sesión

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
 }
